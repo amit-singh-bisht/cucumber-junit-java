cb6de9d969f9b07d1b23db48fe4cf4fe30b2301b
Amit Singh Bisht <69075588+amit-singh-bisht@users.noreply.github.com>
2022-11-22 15:04:22 +0530

    Update Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -6,12 +6,12 @@ public class Hooks {
 	
 	@Before
 	public void initialize_driver(Scenario scenario) {
-		System.out.println("Before hook");
-		System.out.println(scenario.getName());
+		System.out.println("Scenario same: " + scenario.getName());
 	}
 	
 	@After
 	public void quit_driver(Scenario scenario) {
-		System.out.println("After hook");
+		String scenarioStatus = scenario.isFailed() ? "failed" : "passed";
+		System.out.println("Scenario status: " + scenarioStatus);
 	}
 }
